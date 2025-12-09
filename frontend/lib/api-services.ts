76ee0e5e17a4c58eb2cda8378da4235ebcef5122
Sajjad Matin <sajjadmatinm@gmail.com>
2025-12-09 10:56:04 +0430

    change the base URL

--- frontend/lib/api-services.ts
+++ frontend/lib/api-services.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000/api",
+  baseURL: "https://webify-software-company.onrender.com",
   withCredentials: true,
 });
